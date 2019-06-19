9e50e6367355e2c8afdbf27bb2cc73c6a7a3bb69
Jonathan Kirst <jskirst@gmail.com>
2019-06-19 13:48:48 -0700

    wip

--- lib/cucumber/sammies/step_definitions/environment_steps.rb
+++ lib/cucumber/sammies/step_definitions/environment_steps.rb
@@ -7,9 +7,9 @@ Given /^the "(.*)" ENV should be set to "(.*)"$/ do |key, value|
 end
 
 Then /^"(.*)" should raise a "(.*)" error$/ do |expression, error|
-  expect { send(expression) }.to raise_error(/#{error}/)
+  expect { eval(expression) }.to raise_error(/#{error}/)
 end
 
 Then /^"(.*)" should not raise a "(.*)" error$/ do |expression, error|
-  expect { send(expression) }.not_to raise_error(/#{error}/)
+  expect { eval(expression) }.not_to raise_error(/#{error}/)
 end
